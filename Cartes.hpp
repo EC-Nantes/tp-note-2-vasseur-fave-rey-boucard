8ca3176ca2e9f3ceea3f8b87fd9b613ea2db0286
FAVE <jonathan.fave28@gmail.com>
2023-03-10 11:44:07 +0100

    Add carte class

---
+++ Cartes.hpp
@@ -0,0 +1,36 @@
+/*
+ * Cartes.hpp
+ *
+ *  Created on: 10 Mar. 2023
+ *      Author: REY Maxime, BOUCARD Romain, VASSEUR François, FAVE Jonathan
+ */
+
+#ifndef CARTES_HPP
+#define CARTES_HPP
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Cartes
+{
+private:
+    //string couleur;
+    //string valeur;
+
+public:
+    Cartes(/* args */);
+    ~Cartes();
+};
+
+Cartes::Cartes(/* args */)
+{
+}
+
+Cartes::~Cartes()
+{
+}
+
+
+#endif //CARTES_HPP
