f806db38198be324b0291252f39839b95a116569
FAVE <jonathan.fave28@gmail.com>
2023-03-10 11:47:56 +0100

    Add cartes class

---
+++ Cartes.cpp
@@ -0,0 +1,39 @@
+#include "Cartes.hpp"
+
+/* Constructor */
+
+Carte::Carte(){
+
+}
+
+/*----------------------------------------------------------*/
+
+/* Methods */
+
+string Carte::Effet(){
+
+}
+
+/*----------------------------------------------------------*/
+
+/* Getters */
+
+string Carte::getCouleur() const{
+    return this->couleur;
+}
+
+string Carte::getValeur() const{
+    return this->valeur;
+}
+
+/*----------------------------------------------------------*/
+
+/* Setters */
+
+string Carte::setCouleur(){
+    
+}
+
+string Carte::setValeur(){
+
+}
